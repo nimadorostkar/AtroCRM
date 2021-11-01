970407d0c2b5c31aa14be52d130f999f999f4a14
nimadorostkar <nimadorostkar97@gmail.com>
2021-11-01 16:45:10 +0330

    jDateTimeField

--- apps/home/models.py
+++ apps/home/models.py
@@ -14,10 +14,11 @@ from django_jalali.db import models as jmodels
 class Product(models.Model):
     available = models.BooleanField(default=True, verbose_name = "موجود" )
     name = models.CharField(max_length=200, unique=True, verbose_name = "نام")
+    inventory = models.integerfield(default='1', verbose_name = "موجودی")
     price = models.CharField(max_length=200, verbose_name = "قیمت ( ریال )")
     description = models.TextField(max_length=900,null=True, blank=True,verbose_name = "توضیحات")
     image = models.ImageField(upload_to='media', default='media/Default.png', verbose_name = "تصویر")
-    date_created = jmodels.jDateField(auto_now_add=True, verbose_name = "تاریخ")
+    date_created = jmodels.jDateTimeField(auto_now_add=True, verbose_name = "تاریخ")
 
     def __str__(self):
         return self.name
@@ -51,9 +52,9 @@ class Customer(models.Model):
     name = models.CharField(max_length=200, null=True,verbose_name = "نام")
     phone = models.CharField(max_length=200, null=True,verbose_name = "تلفن")
     additional_information = models.TextField(max_length=1000,null=True, blank=True,verbose_name = "اطلاعات تکمیلی")
-    #substantial = models.BooleanField(default=False, verbose_name = " قابل توجه " )
+    substantial = models.BooleanField(default=False, verbose_name = " قابل توجه " )
     product_tag = models.ManyToManyField(Product, blank=True, verbose_name = "تگ محصولی")
-    date_created = jmodels.jDateField(auto_now_add=True, verbose_name = "تاریخ")
+    date_created = jmodels.jDateTimeField(auto_now_add=True, verbose_name = "تاریخ")
 
     def __str__(self):
         return self.name
@@ -88,7 +89,7 @@ class Order_request(models.Model):
     discount = models.IntegerField(default='0', verbose_name = "درصد تخفیف" )
     CHOICES = ( ('تکمیل شده','تکمیل شده'), ('لغو شده','لغو شده'), ('دریافت پیش پرداخت','دریافت پیش پرداخت'), ('در حال بررسی','در حال بررسی') )
     status = models.CharField(max_length=30,choices=CHOICES, default='در حال بررسی', verbose_name = "وضعیت")
-    date_created = jmodels.jDateField(auto_now_add=True, verbose_name = "تاریخ")
+    date_created = jmodels.jDateTimeField(auto_now_add=True, verbose_name = "تاریخ")
 
 
     def __str__(self):
@@ -112,7 +113,7 @@ class Order_steps(models.Model):
     CHOICES = ( ('اول','اول'), ('دوم','دوم'), ('سوم','سوم'), ('چهارم','چهارم'), ('پنجم','پنجم'), ('ششم','ششم'), ('هفتم','هفتم'), ('هشتم','هشتم'), ('نهم','نهم'), ('دهم','دهم') )
     step=models.CharField(max_length=20,choices=CHOICES, verbose_name = "گام")
     description = models.TextField(max_length=1000, null=True, blank=True, verbose_name = "توضیحات")
-    date_created = jmodels.jDateField(auto_now_add=True, verbose_name = "تاریخ")
+    date_created = jmodels.jDateTimeField(auto_now_add=True, verbose_name = "تاریخ")
 
 
     def __str__(self):
