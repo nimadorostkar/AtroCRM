18642403002e70f4a3faf5a138432ab25da18558
nimadorostkar <nimadorostkar97@gmail.com>
2021-11-30 23:06:59 +0330

    prj done

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['www.crm.atrotech.ir', 'crm.atrotech.ir']
+#ALLOWED_HOSTS = ['www.crm.atrotech.ir', 'crm.atrotech.ir']
+SECRET_KEY = '1^%12q4s7^@86prla^qv16*i0-&ml0zx8e+t^(hm66-q&v5#+3'
 
 # Application definition
 
@@ -78,11 +79,11 @@ DATABASES = {
 '''
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'atrotech_CRM',
         'USER': 'atrotech_admin',
         'PASSWORD': '123456atrotech!',
-        'HOST': 'crm.atrotech.ir',  
+        'HOST': 'crm.atrotech.ir',
         'PORT': '3306',
         "OPTIONS": {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
@@ -167,27 +168,20 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 #############################################################
 # SRC: https://devcenter.heroku.com/articles/django-assets
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-#STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
-STATIC_ROOT  = '/home/atrotech/public_html/crm.atrotech.ir/staticfiles'  
+STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 # Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = (os.path.join(CORE_DIR, 'apps/static'),)
-STATICFILES_DIRS = ('/home/atrotech/public_html/crm.atrotech.ir/static'),
-
-
-#MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/home/atrotech/public_html/crm.atrotech.ir/media'
+STATICFILES_DIRS = (
+    os.path.join(CORE_DIR, 'apps/static'),
+)
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
 
 
 
-
-#############################################################
-#############################################################
+#
